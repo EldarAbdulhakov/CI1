fc5fff51d7b2fc4ad7c3741dba9d983c6ab5e100
EldarAbdulhakov <braid2012@gmail.com>
2025-10-28 00:34:41 +0300

    Add project on abdulhakov-probationCI5 branch

--- src/test/java/page/MainPage.java
+++ src/test/java/page/MainPage.java
@@ -95,8 +95,8 @@ public class MainPage {
     public MainPage chooseYellow() {
         WebElement element = driver.findElement(By.xpath("//label[@for='color3']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        getWait10().until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='color3']")));
-        element.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+
         return this;
     }
 
